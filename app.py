a098bbd11170dc2d1011e5b8269d37aa8cf682cb
Blanco <adillabiad@gmail.com>
2016-08-07 12:06:28 -0400

    New: findById(), removeById(), addedToList()

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import pymongo
 import json
 from bson.objectid import ObjectId
 from bson.json_util import dumps
-from bottle import request, response
+from bottle import request, response, route
 
 url = "mongodb://Localhost:27017"
 connection = pymongo.MongoClient(url)
 db = connection.winedb
 wines = db.wines
 
-@bottle.get('/wines')
+
+
+# GET wines list
+@route('/wines', method='GET')
 def findAll():
     items = wines.find()
     result = []
@@ -26,9 +29,29 @@ def findAll():
             'description': item['description'],
             'picture': item['picture']
             })
-            
     return dumps(result)
-            
+
+# GET wine by id    
+@route('/wines/:id', method='GET')
+def findById(id):
+    item = wines.find_one({'_id': ObjectId(id)})
+    return dumps(item)
+    
+# POST a wine at the list
+@route('/wines', method='POST')
+def addOneToList():
+    data = request.body.read()
+    item = json.loads(data)
+    print item
+    wines.insert_one(item)       
+    
+# DELETE wine from the list
+@route('/wines/:id', method='DELETE')
+def removeById(id):
+    wines.remove({'_id': ObjectId(id)})
+    return "item deleted"
+    
+
 
 bottle.debug(True)
 bottle.run(host = 'localhost', port = 8080, reloader = True)
