18708cff228d3ef72742db3fdfb4ee595231cfbc
Blanco <adillabiad@gmail.com>
2016-08-07 09:09:55 -0400

    First commit findAll()

---
+++ app.py
@@ -0,0 +1,34 @@
+import bottle
+import pymongo
+import json
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from bottle import request, response
+
+url = "mongodb://Localhost:27017"
+connection = pymongo.MongoClient(url)
+db = connection.winedb
+wines = db.wines
+
+@bottle.get('/wines')
+def findAll():
+    items = wines.find()
+    result = []
+    
+    for item in items:
+        result.append({
+            '_id' : str( item['_id']), 
+            'name': item['name'], 
+            'year': item['year'],
+            'grapes' : item['grapes'],
+            'country': item['country'],
+            'region' : item['region'],
+            'description': item['description'],
+            'picture': item['picture']
+            })
+    response.content_type = 'application/json'
+    return dumps(result)
+            
+
+bottle.debug(True)
+bottle.run(host = 'localhost', port = 8080, reloader = True)
